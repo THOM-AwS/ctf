d746206a64a544b3a17c3bba8d7b88045dfdf9db
Thomas Hamer <thomas_hamer@outlook.com>
2023-09-23 13:29:51 +1000

    ignorefile

---
+++ ctf.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ctfd" {
+  source                            = "1nval1dctf/ctfd/aws"
+  db_deletion_protection            = false
+  elasticache_cluster_instance_type = "cache.t2.micro"
+  elasticache_cluster_instances     = 2
+  db_serverless                     = true
+}
