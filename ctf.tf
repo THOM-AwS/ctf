65612845670e9771922790d424d9b783c82402fe
Thomas Hamer <thomas_hamer@outlook.com>
2023-09-25 19:17:13 +1000

    update module

--- ctf.tf
+++ ctf.tf
@@ -14,8 +14,14 @@ provider "aws" {
 
 module "ctfd" {
   source                            = "1nval1dctf/ctfd/aws"
+  ctf_domain                        = "wctf.hamer.cloud"
+  create_cdn                        = true
+  ctf_domain_zone_id                = "Z01871282UNAOFQSTNCVD"
+  https_certificate_arn             = "arn:aws:acm:us-east-1:490638925706:certificate/7bba1b38-d28c-4665-aa2d-456785dc1b76"
+  force_destroy_challenge_bucket    = true
+  db_cluster_instance_type          = "db.t2.small"
   db_deletion_protection            = false
   elasticache_cluster_instance_type = "cache.t2.micro"
   elasticache_cluster_instances     = 2
-  db_serverless                     = true
+  force_destroy_log_bucket          = true
 }
